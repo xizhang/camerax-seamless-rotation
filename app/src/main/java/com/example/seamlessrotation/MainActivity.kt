1af361da8d76241b0d6052a2b7c9d227a268b167
Xi Zhang <xizh@google.com>
2022-11-30 08:05:05 -0800

    Make the app full screen

--- app/src/main/java/com/example/seamlessrotation/MainActivity.kt
+++ app/src/main/java/com/example/seamlessrotation/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.seamlessrotation
 
 import android.Manifest
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
-
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,12 +21,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makeFullScreen()
         setContentView(R.layout.activity_main)
         if (isPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
         }
+    }
+
+    private fun makeFullScreen() {
+        supportActionBar?.hide()
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 
     private fun startCamera() {
@@ -33,6 +48,7 @@ class MainActivity : AppCompatActivity() {
             cameraProvider = cameraProvideFuture.get()
             val preview = Preview.Builder().build()
             preview.setSurfaceProvider(previewView.surfaceProvider)
+            previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
             cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview)
         }, mainExecutor)
     }
